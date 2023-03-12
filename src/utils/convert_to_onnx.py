d340d602252fef460476a5fe6cad097eb15b4613
harrisDLEng <harris47naeem@gmail.com>
2023-03-12 22:37:01 +0500

    convert pytorch to onnx

---
+++ src/utils/convert_to_onnx.py
@@ -0,0 +1,15 @@
+from .pytorch_model import Classifier, BasicBlock
+from .preprocess import preprocess_numpy
+import torch 
+from PIL import Image
+
+def convert_pytorch_to_onnx(pytorch_model : Classifier, img : Image, onnx_model_name : str):
+    pytorch_model.load_state_dict(torch.load("./pytorch_model_weights.pth"))
+
+    pytorch_model.eval()
+
+    img = Image.open("./n01440764_tench.jpeg")
+    input_tensor = preprocess_numpy(img).unsqueeze(0) 
+    forward_pass = pytorch_model.forward(input_tensor)
+    
+    torch.onnx.export(pytorch_model, input_tensor, './models/'+onnx_model_name, input_names=['inp'], output_names=['res'])
