d7628bfe51e5dfea5b599d9f20e2301844c2174a
harrisDLEng <harris47naeem@gmail.com>
2023-03-12 22:37:55 +0500

    fixes to onnx conversion

--- src/utils/convert_to_onnx.py
+++ src/utils/convert_to_onnx.py
@@ -10,6 +10,5 @@ def convert_pytorch_to_onnx(pytorch_model : Classifier, img : Image, onnx_model_
 
     img = Image.open("./n01440764_tench.jpeg")
     input_tensor = preprocess_numpy(img).unsqueeze(0) 
-    forward_pass = pytorch_model.forward(input_tensor)
     
-    torch.onnx.export(pytorch_model, input_tensor, './models/'+onnx_model_name, input_names=['inp'], output_names=['res'])
+    torch.onnx.export(pytorch_model, input_tensor, './models/'+onnx_model_name, input_names=['input_tensor'], output_names=['output_tensor'])
