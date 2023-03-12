f3df9953b9f252a50b7d07e3bd10f9302ae0332a
harrisDLEng <harris47naeem@gmail.com>
2023-03-13 00:18:59 +0500

    Minor changes to app.py

--- src/app.py
+++ src/app.py
@@ -1,31 +1,17 @@
-from torchvision import transforms
+from utils.model import Onnxmodel
 
-from utils.preprocess import preprocess_numpy
-import onnxruntime as runtime
-import numpy as np
-from PIL import Image
 
 def init(model_name : str = "./models/pytorch_onnx.onnx"):
-    global session
-    session = runtime.InferenceSession(model_name)
+    global onnx 
+    onnx = Onnxmodel(model_name)
 
 def inference(model_inputs:dict):
-    global session
+    global onnx
 
     # Parse out your arguments
     prompt = model_inputs.get('prompt', None)
     if prompt == None:
         return {'message': "No prompt provided"}
 
-    img = Image.open(prompt)
-    input_tensor = preprocess_numpy(img).unsqueeze(0)
-    label_name = session.get_outputs()[0].name
-
-    convert_tensor = transforms.ToTensor()
-
-    pred = session.run([label_name], {input_tensor: np.asarray(input_tensor)})[0]
-    pred = convert_tensor(pred)
-
+    pred = onnx.inference(prompt)
     return pred
-
-
